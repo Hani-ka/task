df272529897624f53ed0a43a2e66f18ffa0c852a
HANI K <hain.ka@outlook.com>
2020-01-13 13:36:26 +0800

    Footer and responsive

--- Js/scripts.js
+++ Js/scripts.js
@@ -2,10 +2,14 @@ $(document).ready(function(){
     $(".latest-section a").hover(
         function(){
             $(this).parent().prev().css("color","#000");
+            $(this).parent().parent().css("background-color","#rgb(228, 222, 222)");
     },
     function(){
-        //$(".latest-section p").css("color","#fff");
-        $(this).parent().prev().css("color","#000");
+        $(this).parent().prev().css("color","#fff");
+
     }
     );
+    $('#btn').click(function() {
+        $(".header-form").toggle();
+      });
 });
